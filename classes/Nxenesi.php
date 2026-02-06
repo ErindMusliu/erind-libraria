a6355f0d1d4cdf002003aba5a4d2ba5c864ef8c1
ErindMusliu <erindmusliu.11@gmail.com>
2026-02-06 17:47:29 +0100

    E bera qe te ruhen fotot

--- classes/Nxenesi.php
+++ classes/Nxenesi.php
@@ -3,8 +3,14 @@
 include('Database.php');
 
 class Nxenesi extends Database{
-    public function saveNxenesi($emri,$mbiemri,$klasa){
-        $sql = "insert into nxenesit(emri,mbiemri,klasa) value('$emri','$mbiemri','$klasa')";
+    public function saveNxenesi($emri,$mbiemri,$klasa,$foto){
+        $sql = "insert into nxenesit(emri,mbiemri,klasa,foto) value('$emri','$mbiemri','$klasa','$foto')";
+
+        return $this->conn->query($sql);
+    }
+
+    public function index(){
+        $sql = "select * from nxenesit";
 
         return $this->conn->query($sql);
     }
--- nxenesi-shto.php
+++ nxenesi-shto.php
@@ -5,6 +5,27 @@ include('classes/Nxenesi.php');
 if($_SERVER['REQUEST_METHOD'] == "POST"){
     $error = [];
 
+    // Foto
+
+    if(isset($_FILES['foto']) && $_FILES['foto']['error'] == 0){
+        $file_name = $_FILES['foto']['name'];
+        $file_size = $_FILES['foto']['size'];
+        $file_tmp = $_FILES['foto']['tmp_name'];
+        $file_type = $_FILES['foto']['type'];
+
+        $extensions = ['png','jpg','jpeg'];
+        $file_ext = strtolower(pathinfo($file_name,PATHINFO_EXTENSION));
+        $new_file = uniqid("profile",true).".".$file_ext;
+
+        if(!move_uploaded_file($file_tmp,"image/" . $new_file)){
+            die("Gabim gjate ruajtjes se image");
+        }
+    }
+
+    else{
+        $error[] = "Ju lutem futeni nje foto tuajen!";
+    }
+
     // Emri
 
     if(isset($_POST['emri']) && !empty($_POST['emri'])){
@@ -54,6 +75,6 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
     else{
         $nxenesi = new Nxenesi();
-        $nxenesi->saveNxenesi($emri,$mbiemri,$klasa);
+        $nxenesi->saveNxenesi($emri,$mbiemri,$klasa,$new_file);
     }
 }
