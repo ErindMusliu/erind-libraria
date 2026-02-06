0e8d7cc69bb820f3dfe1ffcc54720a42188724ad
ErindMusliu <erindmusliu.11@gmail.com>
2026-02-06 18:01:35 +0100

    Update 2

---
+++ nxenesi.php
@@ -0,0 +1,35 @@
+<?php
+
+include('header.php');
+
+?>
+
+<div class="container" style="min-height: 83vh;">
+    <div class="row justify-content-center mt-5">
+        <?php 
+        include('classes/Nxenesi.php');
+        $nxenesi = new Nxenesi();
+        $nxenesit = $nxenesi->index();
+
+        foreach($nxenesit as $n){
+            echo "<div class='col-md-3'>";
+            echo "<div class='card p-3 justify-content-center align-items-center'>";
+            echo "<img src='image/".$n['foto']."' alt='' class='card-img-top w-50 rounded-5'>";
+            echo "<div class='card-body align-items-center'>";
+            echo "<h3 class='card-title'>".$n['emri']." ".$n['mbiemri']."</h3>";
+            echo "<h4 class='card-title text-center'>".$n['klasa']."</h4>";
+            echo "<a href='' class='card-link text-warning text-decoration-none'>Edit</a>";
+            echo "<a href='' class='card-link text-danger text-decoration-none'>Delete</a>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+        }
+        ?>
+    </div>
+</div>
+
+<?php 
+
+include('footer.php');
+
+?>
