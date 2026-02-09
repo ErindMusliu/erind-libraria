1f6e4c577e2986502b62fdfa04eb7eae2e8b8604
ErindMusliu <erindmusliu.11@gmail.com>
2026-02-09 17:41:29 +0100

    Shtova disa metoda

---
+++ classes/Libri.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once("Database.php");
+
+class Libri extends Database{
+    public function saveLibri($titulli,$autori,$sasia_totale){
+        $sql = "insert into librat(titulli,autori,sasia_totale) value('$titulli','$autori','$sasia_totale')";
+
+        return $this->conn->query($sql);
+    }
+
+    public function index(){
+        $sql = "select * from librat";
+        
+        return $this->conn->query($sql);
+    }
+
+    public function find($id){
+        $sql = "select * from librat where id='$id'";
+
+        return $this->conn->query($sql);
+    }
+
+    public function update($id,$titulli,$autori,$sasia_totale){
+        $sql = "update librat set titulli='$titulli',autori='$autori',sasia_totale='$sasia_totale' where id='$id'";
+
+        return $this->conn->query($sql);
+    }
+
+    public function delete($id){
+        $sql = "delete from librat where id='$id'";
+
+        return $this->conn->query($sql);
+    }
+}
