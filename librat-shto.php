d10e22b41ad89b43b098cc3e33b6a4fb4f6e3fe2
ErindMusliu <erindmusliu.11@gmail.com>
2026-02-09 17:50:02 +0100

    I morra te dhenat dhe e bera qe te shtohen ne databaze

---
+++ librat-shto.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('classes/Libri.php');
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $error = [];
+
+    if(isset($_POST['titulli']) && !empty($_POST['titulli'])){
+        $titulli = htmlspecialchars(strip_tags(trim($_POST['titulli'])));
+    }
+
+    else{
+        $error[] = "Ju lutem plotesoni fushen e Titullit";
+    }
+
+    if(isset($_POST['autori']) && !empty($_POST['autori'])){
+        $autori = htmlspecialchars(strip_tags(trim($_POST['autori'])));
+    }
+
+    else{
+        $error[] = "Ju lutem plotesoni fushen e Autorit";
+    }
+
+    if(isset($_POST['sasia']) && !empty($_POST['sasia'])){
+        $sasia = htmlspecialchars(strip_tags(trim($_POST['sasia'])));
+    }
+
+    else{
+        $error[] = "Ju lutem plotesoni fushen e Sasise";
+    }
+
+    if(count($error) > 0){
+        foreach($error as $e){
+            echo $e."<br>";
+        }
+    }
+
+    else{
+        $libri = new Libri();
+        $libri->saveLibri($titulli,$autori,$sasia);
+        header("Location:librat.php");
+    }
+}
