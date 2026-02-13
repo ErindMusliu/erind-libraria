58537f78366ed041c87177f6dfe193effb172644
ErindMusliu <erindmusliu.11@gmail.com>
2026-02-13 18:13:59 +0100

    Shtova nje form per furnizime

--- classes/Furnizimi.php
+++ classes/Furnizimi.php
@@ -9,8 +9,8 @@ class Furnizimi extends Database{
         return $this->conn->query($sql);
     }
 
-    public function saveFurnizimi($libri,$sasia_furnizimit,$cmimi_sasi,$data_porosise,$zbritje){
-        $sql = "insert into furnizimi(libri,sasia_furnizimit,cmimi_sasi,data_porosise,zbritje) value('$libri','$sasia_furnizimit','$cmimi_sasi','$data_porosise','$zbritje')";
+    public function saveFurnizimi($libri,$sasia_furnizimit,$cmimi_sasi,$zbritje){
+        $sql = "insert into furnizimi(libri,sasia_furnizimit,cmimi_sasi,zbritje) value('$libri','$sasia_furnizimit','$cmimi_sasi','$zbritje')";
         
         return $this->conn->query($sql);
     }
---
+++ furnizimet-form.php
@@ -0,0 +1,47 @@
+<?php
+
+include('header.php');
+
+?>
+
+<div class="container" style="min-height: 83vh;">
+    <div class="row justify-content-center mt-4">
+        <div class="col-md-6 mt-5">
+            <form action="librat-shto.php" enctype="multipart/form-data" class="shadow-lg p-3 rounded-3" method="post">
+                <h2 class="mb-4">➕ Shto Furnizim</h2>
+
+                <!-- Libri -->
+
+                <div class="mb-3">
+                    <input type="text" name="libri" class="form-control" placeholder="Sheno Librin">
+                </div>
+
+                <!-- Sasia Furnizimit -->
+
+                <div class="mb-3">
+                    <input type="number" name="sasia_furnizimit" class="form-control" placeholder="Sheno Sasine e Furnizimit">
+                </div>
+
+                <!-- Cmimi Sasi -->
+
+                <div class="mb-3">
+                    <input type="number" name="cmimi_sasi" class="form-control" placeholder="Sheno Cmimin">
+                </div>
+
+                <!-- Zbritje -->
+
+                <div class="mb-3">
+                    <input type="number" name="zbritje" class="form-control" placeholder="Zbritje">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php 
+
+include('footer.php');
+
+?>
