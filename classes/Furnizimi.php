6c5b65c5f962f6a18b184ce45e66ef7555d204d2
ErindMusliu <erindmusliu.11@gmail.com>
2026-02-13 18:09:45 +0100

    Shtova nje metode saveFurnizimi

--- classes/Furnizimi.php
+++ classes/Furnizimi.php
@@ -8,4 +8,10 @@ class Furnizimi extends Database{
 
         return $this->conn->query($sql);
     }
+
+    public function saveFurnizimi($libri,$sasia_furnizimit,$cmimi_sasi,$data_porosise,$zbritje){
+        $sql = "insert into furnizimi(libri,sasia_furnizimit,cmimi_sasi,data_porosise,zbritje) value('$libri','$sasia_furnizimit','$cmimi_sasi','$data_porosise','$zbritje')";
+        
+        return $this->conn->query($sql);
+    }
 }
