b6c6297badc1e6ba307d03ff8717f8ed3ef323f7
ErindMusliu <erindmusliu.11@gmail.com>
2026-02-18 18:53:09 +0100

    Update

--- search.php
+++ search.php
@@ -1,1 +1,27 @@
 <?php
+
+include('classes/Libri.php');
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $error = [];
+
+    if(isset($_POST['search']) && !empty($_POST['search'])){
+        $search = htmlspecialchars(strip_tags(trim($_POST['search'])));
+    }
+
+    else{
+        $error[] = "Kerkoni cfare te deshironi";
+    }
+
+    if(count($error) > 0){
+        foreach($error as $e){
+            echo $e;
+        }
+    }
+
+    else{
+        $libri = new Libri();
+        $libri->search($search);
+        header('Location:search-result.php');
+    }
+}
