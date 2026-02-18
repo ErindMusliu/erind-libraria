937e169552643873846981a91a7a0b6c48b5eae8
ErindMusliu <erindmusliu.11@gmail.com>
2026-02-18 19:10:04 +0100

    Update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include('header.php');
 <div class="container" style="min-height: 83vh;">
     <div class="row">
         <div>
-            <h1 class="display-4 text-center mt-5">Miresevini ne Librarine e Shkolles</h1>
+            <h1 class="display-4 text-center mt-5 text-dark">Miresevini ne Librarine e Shkolles</h1>
         </div>
     </div>
 </div>
